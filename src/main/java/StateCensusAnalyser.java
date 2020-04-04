bb0ff20d398d644010b0a4636b7da04194397eb1
iamkunalhere <iamkunalhere@gmail.com>
2020-04-05 00:35:16 +0530

    ADDED: UC1 load Indian State Census data from CSV file

---
+++ src/main/java/StateCensusAnalyser.java
@@ -0,0 +1,37 @@
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Paths;
+import java.util.Iterator;
+
+import static java.nio.file.Files.newBufferedReader;
+
+public class StateCensusAnalyser {
+
+    public static void main(String[] args) throws IOException {
+        StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
+        stateCensusAnalyser.loadData();
+    }
+    private static final String DATA_CSV_FILE_PATH = "./src/test/resources/StateCensusData.csv";
+    public void loadData() throws IOException{
+
+            try(Reader reader = newBufferedReader(Paths.get(DATA_CSV_FILE_PATH)); ){
+                CsvToBean<CSVStateCensus> csvStateCensusBeanObj = new CsvToBeanBuilder(reader)
+                        .withType(CSVStateCensus.class)
+                        .withIgnoreLeadingWhiteSpace(true)
+                        .build();
+
+                Iterator<CSVStateCensus> csvStateCensusIterator = csvStateCensusBeanObj.iterator();
+                while (csvStateCensusIterator.hasNext()) {
+                    CSVStateCensus csvStateCensus = csvStateCensusIterator.next();
+                    System.out.println("State: " + csvStateCensus.getState());
+                    System.out.println("Population: " + csvStateCensus.getPopulation());
+                    System.out.println("AreaInSquareKm: " + csvStateCensus.getAreaInSqKm());
+                    System.out.println("DensityPerSquareKm: " + csvStateCensus.getDensityPerSqKm());
+                    System.out.println("----------------------------");
+                }
+            }
+        }
+}
