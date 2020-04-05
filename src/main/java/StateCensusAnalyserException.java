5f4f9ac77bfd189763093ce4ae36a6d3ed892bca
iamkunalhere <iamkunalhere@gmail.com>
2020-04-05 14:17:15 +0530

    ADDED: TC 1.3 if file Type is incorrect throws exception

--- src/main/java/StateCensusAnalyserException.java
+++ src/main/java/StateCensusAnalyserException.java
@@ -1,6 +1,6 @@
 public class StateCensusAnalyserException extends Exception{
     public enum exceptionType {
-        FILE_NOT_FOUND
+        FILE_NOT_FOUND,
     }
     exceptionType exceptionTypeObject;
     public StateCensusAnalyserException(exceptionType exceptionTypeObject) {
--- src/test/java/StateCensusAnalyserTest.java
+++ src/test/java/StateCensusAnalyserTest.java
@@ -4,6 +4,10 @@ import java.io.IOException;
 
 public class StateCensusAnalyserTest {
     private static String DATA_CSV_FILE_PATH = "./src/test/resources/StateCensusData.csv";
+    private static String IMPROPER_FILE_NAME = "./src/test/resources/stateensusata.csv";
+    private static String IMPROPER_FILE_TYPE = "./src/test/resources/StateCensusData.txt";
+
+
     @Test
     public void totalRecordsFromCSV_whenMatched_shouldReturnTrue() throws StateCensusAnalyserException, IOException {
         StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser(DATA_CSV_FILE_PATH);
@@ -13,7 +17,17 @@ public class StateCensusAnalyserTest {
     @Test
     public void givenFileName_whenImproper_shouldThrowException() throws IOException {
         try {
-            StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser(DATA_CSV_FILE_PATH);
+            StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser(IMPROPER_FILE_NAME);
+            stateCensusAnalyser.loadData();
+        }
+        catch (StateCensusAnalyserException e) {
+            Assert.assertEquals(StateCensusAnalyserException.exceptionType.FILE_NOT_FOUND,e.exceptionTypeObject);
+        }
+    }
+    @Test
+    public void givenFileType_whenIncorrect_shouldThrowException() throws IOException {
+        try {
+            StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser(IMPROPER_FILE_TYPE);
             stateCensusAnalyser.loadData();
         }
         catch (StateCensusAnalyserException e) {
