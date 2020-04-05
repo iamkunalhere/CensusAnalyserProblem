989a2f79736646694e9d1805b1eec4b30a0b2806
iamkunalhere <iamkunalhere@gmail.com>
2020-04-05 12:18:05 +0530

    ADDED: TC 1.2 if CSV file is incorrect throws exception

---
+++ src/main/java/StateCensusAnalyserException.java
@@ -0,0 +1,9 @@
+public class StateCensusAnalyserException extends Exception{
+    public enum exceptionType {
+        FILE_NOT_FOUND
+    }
+    exceptionType exceptionTypeObject;
+    public StateCensusAnalyserException(exceptionType exceptionTypeObject) {
+        this.exceptionTypeObject=exceptionTypeObject;
+    }
+}
--- src/test/java/StateCensusAnalyserTest.java
+++ src/test/java/StateCensusAnalyserTest.java
@@ -1,13 +1,23 @@
 import org.junit.Assert;
 import org.junit.Test;
-
 import java.io.IOException;
 
 public class StateCensusAnalyserTest {
+    private static String DATA_CSV_FILE_PATH = "./src/test/resources/StateCensusData.csv";
     @Test
-    public void totalRecordsFromCSV_whenMatched_shouldReturnTrue() throws IOException {
-        StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
+    public void totalRecordsFromCSV_whenMatched_shouldReturnTrue() throws StateCensusAnalyserException, IOException {
+        StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser(DATA_CSV_FILE_PATH);
         int totalRecords =  stateCensusAnalyser.loadData();
         Assert.assertEquals(29,totalRecords);
+    }
+    @Test
+    public void givenFileName_whenImproper_shouldThrowException() throws IOException {
+        try {
+            StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser(DATA_CSV_FILE_PATH);
+            stateCensusAnalyser.loadData();
+        }
+        catch (StateCensusAnalyserException e) {
+            Assert.assertEquals(StateCensusAnalyserException.exceptionType.FILE_NOT_FOUND,e.exceptionTypeObject);
+        }
     }
 }
