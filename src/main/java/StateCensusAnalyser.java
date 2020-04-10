e87edba20734efa14a734e78e13f010c5da7aafd
Kunal Jadhav <iamkunalhere@gmail.com>
2020-04-10 18:09:36 +0530

    REFACTORED: Refactor code as DRY was violated to sort from CSV files

--- src/main/java/StateCensusAnalyser.java
+++ src/main/java/StateCensusAnalyser.java
@@ -12,15 +12,14 @@ import java.util.stream.StreamSupport;
 
 import static java.nio.file.Files.newBufferedReader;
 
-public class StateCensusAnalyser {
-    List<CSVStateCensusPojo> csvStateCensusFileList = null;
-    List<CSVStateCodePojo> csvStateCodeFileList = null;
+public class StateCensusAnalyser <E>{
+    List<E> csvFileList = null;
 
     public int loadIndianCensusData(String csvFilePath) throws StateCensusAnalyserException {
         try (Reader reader = newBufferedReader(Paths.get(csvFilePath));) {
             ICSVBuilder icsvBuilder = CSVBuilderFactory.icsvBuilder();
-            csvStateCensusFileList = icsvBuilder.getFileList(reader, CSVStateCensusPojo.class);
-            return csvStateCensusFileList.size();
+            csvFileList = (List<E>) icsvBuilder.getFileList(reader, CSVStateCensusPojo.class);
+            return csvFileList.size();
         } catch (IOException e) {
             throw new StateCensusAnalyserException(e.getMessage(), StateCensusAnalyserException.exceptionType.FILE_NOT_FOUND);
         } catch (CSVBuilderException e) {
@@ -33,8 +32,8 @@ public class StateCensusAnalyser {
     public int loadStateCodes(String csvFilePath) throws StateCensusAnalyserException {
         try (Reader reader = newBufferedReader(Paths.get(csvFilePath));) {
             ICSVBuilder icsvBuilder = CSVBuilderFactory.icsvBuilder();
-            csvStateCodeFileList = icsvBuilder.getFileList(reader, CSVStateCodePojo.class);
-            return csvStateCodeFileList.size();
+            csvFileList = (List<E>) icsvBuilder.getFileList(reader, CSVStateCodePojo.class);
+            return csvFileList.size();
         } catch (IOException e) {
             throw new StateCensusAnalyserException(e.getMessage(), StateCensusAnalyserException.exceptionType.FILE_NOT_FOUND);
         } catch (CSVBuilderException e) {
@@ -51,45 +50,32 @@ public class StateCensusAnalyser {
     }
 
     public String getSortedStateCensusData() throws StateCensusAnalyserException {
-        if (csvStateCensusFileList.size() == 0 | csvStateCensusFileList == null) {
+        if (csvFileList.size() == 0 | csvFileList == null) {
             throw new StateCensusAnalyserException("No Census Data",StateCensusAnalyserException.exceptionType.NO_CENSUS_DATA);
         }
         Comparator<CSVStateCensusPojo> censusComparator = Comparator.comparing(census -> census.getState());
-        this.sortName(censusComparator);
-        String stateCensusSortedJson = new Gson().toJson(csvStateCensusFileList);
+        this.sort((Comparator<E>) censusComparator);
+        String stateCensusSortedJson = new Gson().toJson(csvFileList);
         return stateCensusSortedJson;
     }
     public String getSortedStateCodeData() throws StateCensusAnalyserException {
-        if (csvStateCodeFileList.size() == 0 | csvStateCodeFileList == null) {
+        if (csvFileList.size() == 0 | csvFileList == null) {
             throw new StateCensusAnalyserException("No Census Data",StateCensusAnalyserException.exceptionType.NO_CENSUS_DATA);
         }
         Comparator<CSVStateCodePojo> stateCodeComparator = Comparator.comparing(census -> census.getStateCode());
-        this.sortCode(stateCodeComparator);
-        String stateCensusSortedJson = new Gson().toJson(csvStateCodeFileList);
+        this.sort((Comparator<E>) stateCodeComparator);
+        String stateCensusSortedJson = new Gson().toJson(csvFileList);
         return stateCensusSortedJson;
     }
 
-    private void sortName(Comparator<CSVStateCensusPojo> censusComparator) {
-        for (int i = 0; i < csvStateCensusFileList.size(); i++) {
-            for (int j = 0; j < csvStateCensusFileList.size() - i - 1; j++) {
-                CSVStateCensusPojo census1 = csvStateCensusFileList.get(j);
-                CSVStateCensusPojo census2 = csvStateCensusFileList.get(j + 1);
+    private void sort(Comparator<E> censusComparator) {
+        for (int i = 0; i < csvFileList.size(); i++) {
+            for (int j = 0; j < csvFileList.size() - i - 1; j++) {
+                E census1 = csvFileList.get(j);
+                E census2 = csvFileList.get(j + 1);
                 if (censusComparator.compare(census1, census2) > 0) {
-                    csvStateCensusFileList.set(j, census2);
-                    csvStateCensusFileList.set(j + 1, census1);
-                }
-
-            }
-        }
-    }
-    private void sortCode(Comparator<CSVStateCodePojo> censusComparator) {
-        for (int i = 0; i < csvStateCodeFileList.size(); i++) {
-            for (int j = 0; j < csvStateCodeFileList.size() - i - 1; j++) {
-                CSVStateCodePojo census1 = csvStateCodeFileList.get(j);
-                CSVStateCodePojo census2 = csvStateCodeFileList.get(j + 1);
-                if (censusComparator.compare(census1, census2) > 0) {
-                    csvStateCodeFileList.set(j, census2);
-                    csvStateCodeFileList.set(j + 1, census1);
+                    csvFileList.set(j, census2);
+                    csvFileList.set(j + 1, census1);
                 }
 
             }
